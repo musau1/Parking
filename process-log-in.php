f694ab8f266af404f921565ae18487ea4ac79d50
musau1 <59249364+musau1@users.noreply.github.com>
2021-08-19 14:35:32 +0300

    Add files via upload

---
+++ process-log-in.php
@@ -0,0 +1,51 @@
+<?php
+
+require('inc\dbcon.php');
+
+// username and password sent from form
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+// To protect MySQL injection (more detail about MySQL injection)
+$email = stripslashes($email);
+$password = stripslashes($password);
+$email = mysqli_real_escape_string($conn,$email);
+$password = mysqli_real_escape_string($conn,$password);
+
+$sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+$result= mysqli_query($conn, $sql);
+
+// Mysql_num_row is counting table row
+$count= mysqli_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+
+if($count==1){
+//Name variable
+while($row = mysqli_fetch_assoc($result))
+{
+   $name = $row["name"];
+   $level=$row["level"];
+} if ($level==0){
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+// Start the session
+session_start();
+// Set session variables
+$_SESSION["email"] = $email;
+$_SESSION["password"] = $password;
+$_SESSION["name"] = $name;
+//echo "Session variables are set.";
+header("location:success-log-in.php");
+}
+else  if($level==1){
+	session_start();
+// Set session variables
+$_SESSION["email"] = $email;
+$_SESSION["password"] = $password;
+$_SESSION["name"] = $name;
+header("location:admin/admin-welcom.php");
+}
+else {
+echo "Wrong Username or Password";
+}}
+?>
